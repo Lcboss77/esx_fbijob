4076383dbbb3619a8369c5d796ab8182d9f39af1
Lcboss77 <78363850+Lcboss77@users.noreply.github.com>
2021-02-01 16:46:52 +0200

    Add files via upload

---
+++ esx_FBI/esx_FBI.sql
@@ -0,0 +1,23 @@
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_FBI', 'FBI', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_FBI', 'FBI', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_FBI', 'FBI', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('FBI', 'FBI')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('FBI',0,'recruit','Rookie',20,'{}','{}'),
+	('FBI',1,'officer','Caption',40,'{}','{}'),
+	('FBI',2,'sergeant','Hunter',60,'{}','{}'),
+	('FBI',3,'lieutenant','Barvo',85,'{}','{}'),
+	('FBI',4,'boss','Aplha',100,'{}','{}')
+;
